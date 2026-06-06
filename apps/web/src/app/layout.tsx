ec3839e7543ca8ed2a08ec6f7de5b8ab769d68e3
pandatosca <pixelcase@gmail.com>
2026-06-06 12:22:23 +0700

    Update layout.tsx

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,27 +4,25 @@ import "./global.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-	title: "Anything App",
-	description: "Created with Anything",
-	icons: {
-		icon: "/favicon.png",
-	},
+  title: "SatpamPro GDR",
+  description: "Created with Anything",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-	return (
-		<html lang="en">
-			<head>
-				<link
-					rel="stylesheet"
-					href="/fontawesome/releases/v6.3.0/css/pro.min.css?token=2c15cc0cc7"
-				/>
-			</head>
-			<body>
-				<Providers>
-					{children}
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="/fontawesome/releases/v6.3.0/css/pro.min.css?token=2c15cc0cc7"
+        />
+      </head>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
